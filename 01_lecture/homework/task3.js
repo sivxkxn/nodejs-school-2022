160c9186eb4fbc6c5bad4ae36f26118040942ae3
sivxkxn <sivxkxn@gmail.com>
2022-02-10 02:24:53 +0200

    task3

--- 01_lecture/homework/task3.js
+++ 01_lecture/homework/task3.js
@@ -1,3 +1,13 @@
-function searchSerial(arr, val){
-    
+function searchSerial(arr, val) {
+  let counter = 0;
+  let arrayOfCounts = [];
+  for (let i = 0; i < arr.length; i++) {
+    while (arr[i] === val) {
+      counter++;
+      i++;
+    }
+    arrayOfCounts.push(counter);
+    counter = 0;
+  }
+  return arrayOfCounts.sort((a, b) => a - b).pop();
 }
